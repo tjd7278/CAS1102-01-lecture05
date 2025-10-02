7e1b0f2a9be06711d4c29bb79865a78a787e3ec2
minsung Kim <114562280+tjd7278@users.noreply.github.com>
2025-10-02 17:30:38 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -29,8 +29,8 @@ void decode_steganography(int image_data[], int data_size, std::string key) {
      */
     bool B = false;
     while (B == false){
-        for (int i=0; i<strlen(std::string key); i++) {
-            int jump = (int)std::string key[i];
+        for (int i=0; i<key.length(); i++) {
+            int jump = (int)key[i];
             curentIndex += jump;
             int secret_value = image_data[currentIndex];
             if (secret_value == 0){
