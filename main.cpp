01b0fcdee879ab9c004d7f8f73f398c5d4cc6b12
minsung Kim <114562280+tjd7278@users.noreply.github.com>
2025-10-02 17:28:03 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -28,10 +28,9 @@ void decode_steganography(int image_data[], int data_size, std::string key) {
      * - If the value is 0, stop the loop. Otherwise, cast the value to a `char` and print it.
      */
     bool B = false;
-    char key_key[strlen(std::string key)] = std::string key;
     while (B == false){
         for (int i=0; i<strlen(std::string key); i++) {
-            int jump = (int)key_key[i];
+            int jump = (int)std::string key[i];
             curentIndex += jump;
             int secret_value = image_data[currentIndex];
             if (secret_value == 0){
